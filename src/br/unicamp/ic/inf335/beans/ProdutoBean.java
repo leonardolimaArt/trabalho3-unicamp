c540aca70a46067dbfa183db680b4bb2f2d26933
Leonardo Henrique <leonardolima.art@gmail.com>
2025-09-07 15:12:07 -0400

    fix: add fix for bugs

--- src/br/unicamp/ic/inf335/beans/ProdutoBean.java
+++ src/br/unicamp/ic/inf335/beans/ProdutoBean.java
@@ -47,11 +47,11 @@ public class ProdutoBean implements java.io.Serializable, Comparable<ProdutoBean
 	}
 	
 	public ProdutoBean () {
-		codigo = new String();
-		nome = new String();
-	    descricao = new String();
+		codigo = "";
+		nome = "";
+		descricao = "";
 		valor = 0.0;
-		estado = new String();
+		estado = "";
 	}
 	
 	
@@ -68,12 +68,12 @@ public class ProdutoBean implements java.io.Serializable, Comparable<ProdutoBean
 	public int compareTo(ProdutoBean p) {
 		if (valor > p.getValor()) {
 			return 1;
-		} else if (valor > p.getValor()) {
+		} else if (valor < p.getValor()) {
 			return -1;
-		} else
+		}
 		    return 0;
 	}
-	
-	
+
+
 
 }

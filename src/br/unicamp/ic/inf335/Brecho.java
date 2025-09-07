24e7a0e76b16ffe5dec2024defca2a5249280f8c
Leonardo Henrique <leonardolima.art@gmail.com>
2025-09-07 15:12:07 -0400

    fix: add fix for bugs

--- src/br/unicamp/ic/inf335/Brecho.java
+++ src/br/unicamp/ic/inf335/Brecho.java
@@ -1,6 +1,7 @@
 package br.unicamp.ic.inf335;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import br.unicamp.ic.inf335.beans.ProdutoBean;
@@ -10,17 +11,18 @@ public class Brecho {
 	private static ArrayList<ProdutoBean> produtos = new ArrayList<ProdutoBean>();
 	
 	public static void main(String[] args) {
-		ProdutoBean nProg = new ProdutoBean("CD00001","Celular Galaxy S10", "128 Gb, Preto, com Carregador",1250.0,"Poucos riscos, estado de novo.");
-		nProg = new ProdutoBean("CD00002","Prod 2 ...", "Bla Bla Bla",1100.0,"Bla Bla Bla");
-		nProg = new ProdutoBean("CD00003","Prod 3 ...", "Bla Bla Bla",120.0,"Bla Bla Bla");
-		nProg = new ProdutoBean("CD00004","Prod 4 ...", "Bla Bla Bla",1300.0,"Bla Bla Bla");
-		nProg = new ProdutoBean("CD00005","Prod 5 ...", "Bla Bla Bla",9400.0,"Bla Bla Bla");
-		nProg = new ProdutoBean("CD00006","Prod 6 ...", "Bla Bla Bla",1500.0,"Bla Bla Bla");
-		produtos.add(nProg);
+		ArrayList<ProdutoBean> produtos = new ArrayList<>(Arrays.asList(
+				new ProdutoBean("CD00001", "Celular Galaxy S10", "128 Gb, Preto, com Carregador", 1250.0, "Poucos riscos, estado de novo."),
+				new ProdutoBean("CD00002", "Prod 2 ...", "Bla Bla Bla", 1100.0, "Bla Bla Bla"),
+				new ProdutoBean("CD00003", "Prod 3 ...", "Bla Bla Bla", 120.0, "Bla Bla Bla"),
+				new ProdutoBean("CD00004", "Prod 4 ...", "Bla Bla Bla", 1300.0, "Bla Bla Bla"),
+				new ProdutoBean("CD00005", "Prod 5 ...", "Bla Bla Bla", 9400.0, "Bla Bla Bla"),
+				new ProdutoBean("CD00006", "Prod 6 ...", "Bla Bla Bla", 1500.0, "Bla Bla Bla")
+		));
 		
 		// Imprime produtos
-		for (int i=1; i<=produtos.size(); i++) {
-			System.out.println("Codigo = " + produtos.get(i).getCodigo() + " Nome = " + produtos.get(i).getNome() + " Valor = " + produtos.get(i).getValor());
+		for (ProdutoBean p : produtos) {
+			System.out.println("Codigo = " + p.getCodigo() + " Nome = " + p.getNome() + " Valor = " + p.getValor());
 		}
 		
 		// Ordena produtos
@@ -28,17 +30,18 @@ public class Brecho {
 		
 		System.out.println("-------------------- Produtos Ordenados -------------------");
 		// Imprime produtos ordenados
-		for (int i=1; i<=produtos.size(); i++) {
-			System.out.println("Codigo = " + produtos.get(i).getCodigo() + " Nome = " + produtos.get(i).getNome() + " Valor = " + produtos.get(i).getValor());
+		for (ProdutoBean p : produtos) {
+			System.out.println("Codigo = " + p.getCodigo() + " Nome = " + p.getNome() + " Valor = " + p.getValor());
 		}
 		
-		// Calcula Média
+		// Calcula Mï¿½dia
 		Double media = 0.0;
 		int i = 1;
-		while (i<=produtos.size()) {
-			media = produtos.get(i).getValor();
-			i++;
+
+		for (ProdutoBean p : produtos) {
+			media += p.getValor();
 		}
+
 		media = media / i;
 		System.out.println("Media de Valores = " + media);
 	}
